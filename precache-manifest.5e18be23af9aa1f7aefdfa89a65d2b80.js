f191052906c2f21d58f424158f9ea71f6758033c
Carula <carulagarat@gmail.com>
2019-06-06 17:21:50 +0200

    Updates

---
+++ precache-manifest.5e18be23af9aa1f7aefdfa89a65d2b80.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e7698291aff3897efdc2233eba228b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "da4743f6f3840f32ccb7",
+    "url": "/portfolio/static/css/main.f18e18e9.chunk.css"
+  },
+  {
+    "revision": "1dbb4f01d4882b78e731",
+    "url": "/portfolio/static/js/2.71cb0cda.chunk.js"
+  },
+  {
+    "revision": "da4743f6f3840f32ccb7",
+    "url": "/portfolio/static/js/main.b5e34b12.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "36b293a701161767e17f241115b15758",
+    "url": "/portfolio/static/media/iconFont.36b293a7.ttf"
+  },
+  {
+    "revision": "45a4c8a247f18d3183fc1936d88142ab",
+    "url": "/portfolio/static/media/iconFont.45a4c8a2.woff"
+  },
+  {
+    "revision": "9cb8d642fd2ed0e2ca2ac38cb4a4e71b",
+    "url": "/portfolio/static/media/iconFont.9cb8d642.eot"
+  },
+  {
+    "revision": "f9e795f34d5d829761d6ac5bae1a06bf",
+    "url": "/portfolio/static/media/iconFont.f9e795f3.svg"
+  }
+]);
